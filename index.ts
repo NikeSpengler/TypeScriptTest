6975f695d78b5de247b651c14601b2eed6f27bac
Nike Spengler <32640656+NikeSpengler@users.noreply.github.com>
2025-01-14 14:04:42 +0100

    update

--- index.ts
+++ index.ts
@@ -1,3 +1,19 @@
-async function hello() {
-    return "world"
+//string
+let userName: string = "Adam"
+
+//Number
+let age: number = 30
+
+//Boolean
+let isStudent: boolean = true
+
+//Array 
+let numbers: number[] = [1, 2, 3, 4, 5]
+let friends: string[] = ['Adam', 'Bianca', 'Clara']
+
+//Tuple, an arrays more strict cousin. We decide a fixed number of items.
+let person: [string, number] = ['Nike', 33]
+
+function getNameAndAge(): [string, number] {
+    return ['Nike', 33]
 }
